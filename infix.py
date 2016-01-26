2add570370e9c4b657eae31d931216a7eccb0531
Landon Powell <landonpowell@linuxmail.org>
2016-01-25 22:34:57 -0500

    Create infix.py
    Dedicating this parser to my brother in memes, Get52. ;o

---
+++ infix.py
@@ -0,0 +1,20 @@
+x = input("> ").replace('(', ' ( ').replace('(', ' ( ').split()
+
+def calculate(tokens):
+    accumulator = float(tokens.pop(0))
+    while len(tokens):
+        if tokens[0] == '*':
+            tokens.pop(0)
+            accumulator *= float(tokens.pop(0))
+        elif tokens[0] == '/':
+            tokens.pop(0)
+            accumulator /= float(tokens.pop(0))
+        elif tokens[0] == '-':
+            tokens.pop(0)
+            accumulator -= float(tokens.pop(0))
+        elif tokens[0] == '+':
+            tokens.pop(0)
+            accumulator += float(tokens.pop(0))
+    return accumulator
+
+print(calculate(x))
